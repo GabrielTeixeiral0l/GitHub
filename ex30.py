8079a083a77b610fadaf96ce4046c01349d3f6f7
GabrielTeixeiral0l <126079748+GabrielTeixeiral0l@users.noreply.github.com>
2023-11-05 19:08:45 +0000

    segundo commit

---
+++ ex30.py
@@ -0,0 +1,12 @@
+from random import randint
+lista = {randint(1,100) for _ in range(20)}
+par = 0
+impar = 0
+for i in lista:
+    if i % 2 == 0:
+        par+=i
+    else:
+        impar+=i
+print('par: ',par)
+print('impar: ',impar)
+print('valores: ', lista)
